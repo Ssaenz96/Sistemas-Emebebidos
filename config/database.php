05406ff934ec39539d1b6e33bab880e9ff8f4cc5
Ssaenz96 <saul_sesv@hotmail.xom>
2018-11-02 22:33:07 -0600

    second commit

--- config/database.php
+++ config/database.php
@@ -41,11 +41,11 @@ return [
 
         'mysql' => [
             'driver' => 'mysql',
-            'host' => env('DB_HOST', '127.0.0.1'),
+            'host' => env('DB_HOST', 'den1.mysql6.gear.host'),
             'port' => env('DB_PORT', '3306'),
-            'database' => env('DB_DATABASE', 'forge'),
-            'username' => env('DB_USERNAME', 'forge'),
-            'password' => env('DB_PASSWORD', ''),
+            'database' => env('DB_DATABASE', 'profiles1'),
+            'username' => env('DB_USERNAME', 'profiles1'),
+            'password' => env('DB_PASSWORD', 'Xo5GdK8~ZIw-'),
             'unix_socket' => env('DB_SOCKET', ''),
             'charset' => 'utf8mb4',
             'collation' => 'utf8mb4_unicode_ci',
